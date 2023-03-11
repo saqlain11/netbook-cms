3f4de70861445e96dc1af9cc342c92dcadb246b5
saqlain11 <saqlainashrafahc1@gmail.com>
2023-03-11 18:28:28 +0500

    setup test with vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,24 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import type { UserConfig as VitestUserConfigInterface } from 'vitest/config';
+
+const vitestConfig: VitestUserConfigInterface = {
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: 'setupTest.ts',  }
+};
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: vitestConfig.test,
   resolve: {
     alias: {
       "@netbook": path.resolve(__dirname, "./src"),
     },
   },
+  
 });
